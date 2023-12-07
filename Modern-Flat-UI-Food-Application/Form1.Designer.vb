bcabe1e768648dd1054166826cb7741632f4178f
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-12-07 16:49:16 +0700

    create simple form with panel and button

---
+++ Modern-Flat-UI-Food-Application/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Panel3 = New Panel()
+        Button1 = New Button()
+        Panel2 = New Panel()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.DarkRed
+        Panel1.Controls.Add(Panel3)
+        Panel1.Controls.Add(Button1)
+        Panel1.Dock = DockStyle.Left
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(200, 498)
+        Panel1.TabIndex = 0
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.Chartreuse
+        Panel3.Location = New Point(0, 86)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(10, 37)
+        Panel3.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatAppearance.BorderColor = Color.White
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Franklin Gothic Book", 12.0F, FontStyle.Bold)
+        Button1.ForeColor = SystemColors.Control
+        Button1.Image = My.Resources.Resources.dish_icon
+        Button1.Location = New Point(0, 85)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(200, 38)
+        Button1.TabIndex = 0
+        Button1.Text = "  Foods"
+        Button1.TextImageRelation = TextImageRelation.ImageBeforeText
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.Chartreuse
+        Panel2.Dock = DockStyle.Top
+        Panel2.Location = New Point(200, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(683, 31)
+        Panel2.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(883, 498)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+End Class
